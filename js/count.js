b2644049572f9596d4618f633c5c4b6ae65342a1
Creper132 <105077573+Creper132@users.noreply.github.com>
2022-09-10 19:05:58 +0200

    Ulepszenie countera

--- js/count.js
+++ js/count.js
@@ -1,3 +1,3 @@
 $.getJSON("https://api.countapi.xyz/hit/creper132.github.io/visits", function(response) {
-    $("#visits").text(response.value);
+    $("#visits").text("Tak ogólnie ta strona została odwiedzona " + response.value + " razy!");
 });
